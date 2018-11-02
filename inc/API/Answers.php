9bcc701a0b926ee295ef6fa423a38cfa8c0cddf4
Tanner Moushey <tanner@iwitnessdesign.com>
2018-11-02 10:23:13 -0700

    update comment edit/view permission

--- inc/API/Answers.php
+++ inc/API/Answers.php
@@ -182,4 +182,60 @@ class Answers extends WP_REST_Comments_Controller {
 		return $query_params;
 	}
 
+	/**
+	 * Checks if the comment can be read.
+	 *
+	 * @since 4.7.0
+	 *
+	 * @param \WP_Comment      $comment Comment object.
+	 * @param \WP_REST_Request $request Request data to check.
+	 *
+	 * @return bool Whether the comment can be read.
+	 */
+	protected function check_read_permission( $comment, $request ) {
+		if ( ! empty( $comment->comment_post_ID ) ) {
+			$post = get_post( $comment->comment_post_ID );
+			if ( $post ) {
+				if ( $this->check_read_post_permission( $post, $request ) && 1 === (int) $comment->comment_approved ) {
+					return true;
+				}
+			}
+		}
+
+		if ( 0 === get_current_user_id() ) {
+			return false;
+		}
+
+		if ( ! empty( $comment->user_id ) && get_current_user_id() === (int) $comment->user_id ) {
+			return true;
+		}
+
+		return current_user_can( 'edit_comment', $comment->comment_ID );
+	}
+
+	/**
+	 * Checks if a comment can be edited or deleted.
+	 *
+	 * @since 4.7.0
+	 *
+	 * @param object $comment Comment object.
+	 *
+	 * @return bool Whether the comment can be edited or deleted.
+	 */
+	protected function check_edit_permission( $comment ) {
+		if ( 0 === (int) get_current_user_id() ) {
+			return false;
+		}
+
+		if ( ! empty( $comment->user_id ) && get_current_user_id() === (int) $comment->user_id ) {
+			return true;
+		}
+
+		if ( ! current_user_can( 'moderate_comments' ) ) {
+			return false;
+		}
+
+		return current_user_can( 'edit_comment', $comment->comment_ID );
+	}
+
 }
